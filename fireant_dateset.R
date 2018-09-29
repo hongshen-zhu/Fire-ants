8249a910fd6397c0d6bd3300ab994d38de7a7747
Jiawei Fu <fujiaweisteven@hotmail.com>
2018-09-29 11:54:56 -0400

    finish Alabama

--- fireant_dateset.R
+++ fireant_dateset.R
@@ -1,1 +1,27 @@
-# creat the fire ant dataset
+# create the fire ant dataset
+
+library(stringr)
+
+alabama <- "Autauga (1953), Baldwin (1931), Barbour (1958), Bibb (1958), Blount (1966), Bullock (1958), Butler (1958), Calhoun (1958), Chambers (1961), Cherokee (1975), Chilton (1953), Choctaw (1953), Clarke (1939), Clay (1961), Cleburne (1966), Coffee (1962), Colbert (1977), Conecuh (1939), Coosa (1961), Covington (1958), CrenShaw (1938), Cullman (1969), Dale (1938), Dallas (1953), DeKalb (1984), Elmore (1958), Escambia (1939), Etowah (1958), Fayette (1966). Franklin (1975), Geneva (1958), Greene (1958), Hale (1958), Henry (1958), Houston (1958), Jackson (1988), Jefferson (1958), Lamar (1960), Lauderdale (1977), Lawrence (1975), Lee (1958), Limestone (1958), Lowndes (1953), Madison (1984), Marengo (1953), Marion (1966), Marshall (1976), Mobile (1918), Monroe (1939), Montgomery (1953), Morgan (1958), Perry (1933), Pickens (1958), Pike (1953), Randolph (1969), Russell (1958), St.Clair (1960), Shelby (1959), Sumter (1953), Talladega (1959), Tallapoosa (1961), Tuscaloosa (1958), Walker (1958), Washington (1939), Wilcox (1953), Winston (1975)"
+
+split_text <- str_split(alabama, " ") %>%
+  .[[1]] %>%
+  str_replace("\\(","") %>%
+  str_replace("\\)","") %>%
+  str_replace("\\,","")
+
+# how many county-year pair
+n <- length(split_text)/2
+
+# create vectors to save the splited county and year info
+county <- rep(NA,n)
+year <- rep(NA,n)
+
+# put county and year into 2 vectors
+for(i in 1:n){
+  county[i] <- split_text[2*i-1]
+  year[i] <- split_text[2*i]
+}
+
+fire_ant_dat <- data.frame(state="Alabama", county=county,year=year)
+
